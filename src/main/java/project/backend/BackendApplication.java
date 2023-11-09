a27a84aea9111d6099527cde1c251a671cc44010
gabang2 <offbeat1020@naver.com>
2023-11-09 18:06:17 +0900

    chore : 배포시 타임존 변경

--- src/main/java/project/backend/BackendApplication.java
+++ src/main/java/project/backend/BackendApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @SpringBootApplication(
 		exclude = {
@@ -13,6 +16,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 		}
 )
 public class BackendApplication {
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
