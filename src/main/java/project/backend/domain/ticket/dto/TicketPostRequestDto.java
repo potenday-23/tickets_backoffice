32fa4543e798cbefd450e372015efc5d3e79a687
gabang2 <offbeat1020@naver.com>
2023-11-09 23:30:04 +0900

    chore : memo의 글자수제한 100자로 변경

--- src/main/java/project/backend/domain/ticket/dto/TicketPostRequestDto.java
+++ src/main/java/project/backend/domain/ticket/dto/TicketPostRequestDto.java
@@ -7,6 +7,7 @@ import project.backend.domain.ticket.entity.IsPrivate;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Getter
@@ -23,6 +24,7 @@ public class TicketPostRequestDto {
     @Min(value = 0, message = "0보다 작은 값은 별점으로 설정할 수 없습니다.")
     @Max(value = 5, message = "5보다 큰 값은 별점으로 설정할 수 없습니다.")
     public Float rating;
+    @Size(max=10, message = "100자 이내로 입력해주세요.")
     public String memo;
     public String seat;
     public String location;
