19e8f5ccecebed8d85c3f66931905927d0da2a81
gabang2 <offbeat1020@naver.com>
2024-01-10 02:21:10 +0900

    Feat : 로그인시 필터 초기회, 회원 탈퇴 기능

--- src/main/java/project/backend/domain/notice/repository/NoticeRepository.java
+++
@@ -1,7 +0,0 @@
-package project.backend.domain.notice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import project.backend.domain.notice.entity.Notice;
-
-public interface NoticeRepository extends JpaRepository<Notice, Long> {
-}
